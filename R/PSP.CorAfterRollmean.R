6f0f0a868aa4ee8302745b8a6007915309792a5a
Thom Laepple <tlaepple@awi.de>
2016-12-21 13:34:49 +0100

    Added Powerlaw timeseries pair test function

---
+++ R/PSP.CorAfterRollmean.R
@@ -0,0 +1,21 @@
+
+##' @title Numerical correlation of random timeseries with different filtering (running mean)
+##' @param N Number of points of the timeseries
+##' @param betaSignal powerlaw slope of the signal
+##' @param betaNoise powerlaw slope of the noise
+##' @param R expected correlation of the whole timeseries
+##' @return correlation of unfiltered, 10point mean and 50 point mean
+##' @examples
+##' temp <- replicate(1000,PSP.CorAfterRollmean(1000,1,0,0.5))
+##' rowMeans(temp)
+##' PSP.CorUntilF(c(0.5,0.5/10,0.5/50),1,0,1000,0.5) 
+##' @author Thomas Laepple
+##'
+PSP.CorAfterRollmean <- function(N, betaSignal, betaNoise, R)
+{
+    x <- SimulatePowerlawSignalPair(N, betaSignal, betaNoise, R)
+    c.1 <- cor(x$y1, x$y2)
+    c.10 <- cor(rollmean(x$y1, 10), rollmean(x$y2, 10))
+    c.50 <- cor(rollmean(x$y1, 50), rollmean(x$y2, 50))
+    return(c(c.1, c.10, c.50))
+}
---
+++ R/PSP.CorUntilF.R
@@ -0,0 +1,26 @@
+
+
+##' correlation of two timeseries with powerlaw signal and powerlaw noise evaluated until f
+##' this equals the correlation of linearly coupled lowpass filtered powerlaw process
+##'
+##' @title lowpass filtered expected correlation of powerlaw signal pair
+##' @param f frequency until which to integrate
+##' @param betaSignal powerlaw slope of the signal
+##' @param betaNoise powerlaw slope of the noise
+##' @param N Number of points per timeseries
+##' @param r expected correlation of the unfiltered
+##' @return expected correlation of the timeseries
+##' @examples
+##' temp <- replicate(1000,PSP.CorAfterRollmean(1000,1,0,0.5))
+##' rowMeans(temp)
+##' PSP.CorUntilF(f=c(0.5,0.5/10,0.5/50),betaSignal=1,betaNoise=0,N=1000,r=0.5) 
+##' @author Thomas Laepple 
+PSP.CorUntilF <- function(f, betaSignal, betaNoise, N,r) {
+   
+    scale.var.noise <- ((1-r^2)/(r^2)) 
+    var.signal <- PS.VarUntilF(f, betaSignal, N)/ PS.VarUntilF(0.5, 
+        betaSignal, N)
+    var.noise <- PS.VarUntilF(f, betaNoise, N)/ PS.VarUntilF(0.5, 
+        betaNoise, N)
+    sqrt(var.signal/(var.signal + var.noise * scale.var.noise))
+}
