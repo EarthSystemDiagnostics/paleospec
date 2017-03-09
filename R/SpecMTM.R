e49097cabe3e041edd8225008828140f9baca64b
andrewdolman <andydolman@gmail.com>
2017-03-09 14:39:49 +0100

    documentation SpecMTM

--- R/SpecMTM.R
+++ R/SpecMTM.R
@@ -1,6 +1,5 @@
 ##' MTM spectral estimator
-##' calls spec.mtm from library multitaper
-##'  see spec.mtm from library mulitaper ?spec.mtm
+##' calls \code{\link[multitaper]{spec.mtm}} from library multitaper
 ##' @title MTM spectral estimator
 ##' @param timeSeries A time series of equally spaced data, this can be created
 ##' by the ts() function where deltat is specified.
@@ -9,41 +8,49 @@
 ##'          parameter.
 ##' @param nFFT  This function pads the data before computing the fft. nFFT
 ##'          indicates the total length of the data after padding.
-##' @param centre 
-##' @param dpssIN 
-##' @param returnZeroFreq 
-##' @param Ftest 
-##' @param jackknife 
-##' @param jkCIProb 
-##' @param maxAdaptiveIterations 
-##' @param plot 
-##' @param na.action 
-##' @param returnInternals 
-##' @param detrend 
-##' @param bPad 
-##' @param ... 
-##' @return spectra object list(freq,spec,dof)
-##' examples
-##' x<-ts(arima.sim(list(ar = 0.9),1000))
-##' spec<-SpecMTM(x)
-##' LPlot(spec,col="grey")
-##' LLines(LogSmooth(spec),lwd=2)
+##' @param centre
+##' @param dpssIN
+##' @param returnZeroFreq
+##' @param Ftest
+##' @param jackknife
+##' @param jkCIProb
+##' @param maxAdaptiveIterations
+##' @param plot
+##' @param na.action
+##' @param returnInternals
+##' @param detrend
+##' @param bPad
+##' @param ...
+##' @return spectra object list(freq, spec, dof)
+##' @examples
+##' x <- ts(arima.sim(list(ar = 0.9), 1000))
+##' spec <- SpecMTM(x)
+##' LPlot(spec, col='grey')
+##' LLines(LogSmooth(spec), lwd=2)
 ##' @author Thomas Laepple
 ##' @export
-SpecMTM<-function (timeSeries, k=3, nw=2, nFFT = "default", centre = c("Slepian"),
-    dpssIN = NULL, returnZeroFreq = FALSE, Ftest = FALSE, jackknife = FALSE,
-    jkCIProb = 0.95, maxAdaptiveIterations = 100, plot = FALSE,
-    na.action = na.fail, returnInternals = FALSE,detrend=TRUE,bPad=FALSE, ...)
-{
+SpecMTM <- function(timeSeries, k = 3, nw = 2, nFFT = "default",
+  centre = c("Slepian"), dpssIN = NULL, returnZeroFreq = FALSE,
+  Ftest = FALSE, jackknife = FALSE, jkCIProb = 0.95, maxAdaptiveIterations = 100,
+  plot = FALSE, na.action = na.fail, returnInternals = FALSE,
+  detrend = TRUE, bPad = FALSE, ...) {
 
-    if (sum(is.na(timeSeries))>0) stop("missing data")
-    if (!bPad) nFFT=length(timeSeries)
-    if (detrend) timeSeries[]<-lm(timeSeries~seq(timeSeries))$residuals
-    result<-multitaper::spec.mtm(timeSeries=timeSeries, k=k, nw=nw, nFFT=nFFT, centre=centre,
-    dpssIN=dpssIN, returnZeroFreq=returnZeroFreq, Ftest=Ftest, jackknife=jackknife,
-    jkCIProb=jkCIProb,  maxAdaptiveIterations=maxAdaptiveIterations, plot=plot,
-   na.action= na.action, returnInternals=returnInternals, ...)
-    result$dof<-result$mtm$dofs
-    return(result)
+  if (sum(is.na(timeSeries)) > 0)
+    stop("missing data")
+  if (!bPad)
+    nFFT = length(timeSeries)
+  if (detrend)
+    timeSeries[] <- lm(timeSeries ~ seq(timeSeries))$residuals
 
+  result <- multitaper::spec.mtm(timeSeries = timeSeries, k = k,
+    nw = nw, nFFT = nFFT, centre = centre, dpssIN = dpssIN,
+    returnZeroFreq = returnZeroFreq, Ftest = Ftest, jackknife = jackknife,
+    jkCIProb = jkCIProb, maxAdaptiveIterations = maxAdaptiveIterations,
+    plot = plot, na.action = na.action, returnInternals = returnInternals,
+    ...)
+  result$dof <- result$mtm$dofs
+  return(result)
 }
+
+
+
