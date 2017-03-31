98f7c0b1ece6b763945e8d4e47a0b169cccee7da
Thomas Münch <tmuench@awi.de>
2017-03-31 17:12:21 +0200

    'LPlot/LLines': improved code style and edited documentation.

--- R/LLines.R
+++ R/LLines.R
@@ -1,38 +1,42 @@
-##' add Logplot + transparent confidence interval for the spectral plotting
+##' Add a spectrum to an existing log-log spectral plot.
 ##'
-##' 
-##' @title add Logplot + transparent confidence interval for the spectral plotting
-##' @param x  spectra object
-##' @param conf TRUE: Plot confidence interval
+##' This function adds a spectrum to an existing double-logarithmic plot and
+##' optionally adds a transparent confidence interval.
+##' @param x  a spectral object resulting from a call to \code{\link{SpecMTM}}.
+##' @param conf if \code{TRUE} (the default) add a transparent confidence
+##' interval.
 ##' @param bPeriod if \code{TRUE} treat the x-axis values in units of period
 ##'     (inverse frequency). Defaults to \code{FALSE}.
-##' @param col color
-##' @param alpha  transparency
-##' @param removeFirst omit removeFirst values on the low frequency side
-##' @param removeLast omit removeFirst values on the high frequency side
-##' @param ... other parameters to be passed to the line function
-##' @return none
+##' @param col color for the line plot and the confidence interval.
+##' @param alpha transparency level (between 0 and 1) for the confidence
+##' interval. Defaults to \code{0.3}.
+##' @param removeFirst omit \code{removeFirst} values on the low frequency side. 
+##' @param removeLast omit \code{removeLast} values on the high frequency side.
+##' @param ... further graphical parameters passed to \code{lines}.
 ##' @examples
-##' x<-ts(arima.sim(list(ar = 0.9),1000))
-##' spec<-SpecMTM(x)
-##' LPlot(spec,col="grey")
-##' LLines(LogSmooth(spec),lwd=3)
+##' x <- ts(arima.sim(list(ar = 0.9), 1000))
+##' spec <- SpecMTM(x)
+##' LPlot(spec, col = "grey")
+##' LLines(LogSmooth(spec), lwd = 2)
 ##' @author Thomas Laepple
 ##' @export
-LLines<-function(x,conf=TRUE,bPeriod=FALSE,col="black",alpha=0.3,removeFirst=0,removeLast=0,...)
-{
+LLines<-function(x, conf = TRUE, bPeriod = FALSE, col = "black", alpha = 0.3,
+                 removeFirst = 0, removeLast = 0, ...) {
 
-    if (bPeriod) x$freq=1/x$freq
+    if (bPeriod) x$freq <- 1/x$freq
     
-    index<-(removeFirst+1):(length(x$freq)-removeLast)  
-    x$freq<-x$freq[index]
-    x$spec<-x$spec[index]
-    x$lim.1<-x$lim.1[index]
-    x$lim.2<-x$lim.2[index]
+    index <- (removeFirst + 1) : (length(x$freq) - removeLast)
 
-   if (conf) polygon(c(x$freq,rev(x$freq)),c(x$lim.1,
-        rev(x$lim.2)),col=ColTransparent(col,alpha),border=NA)
-    lines(x$freq,x$spec,col=col,...)
+    x$freq  <- x$freq[index]
+    x$spec  <- x$spec[index]
+    x$lim.1 <- x$lim.1[index]
+    x$lim.2 <- x$lim.2[index]
+
+    if (conf) {
+        polygon(c(x$freq, rev(x$freq)), c(x$lim.1, rev(x$lim.2)),
+                col = ColTransparent(col, alpha), border = NA)
+    }
+    
+    lines(x$freq, x$spec, col = col, ...)
+    
 }
-
-
--- R/LPlot.R
+++ R/LPlot.R
@@ -1,33 +1,35 @@
-##' add Logplot + transparent confidence interval for the spectral plotting
+##' Log-log spectral plot.
 ##'
-##' 
-##' @title add Logplot + transparent confidence interval for the spectral plotting
-##' @param x spectra object
-##' @param conf TRUE: Plot confidence interval
+##' This function plots a spectrum on a double-logarithmic scale and optionally
+##' adds a transparent confidence interval.
+##' @param x a spectral object resulting from a call to \code{\link{SpecMTM}}.
+##' @param conf if \code{TRUE} (the default) add a transparent confidence
+##' interval.
 ##' @param bPeriod if \code{TRUE} the x-axis is displayed in units of period
 ##'     (inverse frequency), increasing to the left. Defaults to \code{FALSE}.
-##' @param col color
-##' @param alpha transparency
-##' @param removeFirst omit removeFirst values on the low frequency side
-##' @param removeLast omit removeFirst values on the high frequency side
-##' @param xlab label of x-axes
-##' @param ylab label of y-axes
+##' @param col color for the line plot and the confidence interval.
+##' @param alpha transparency level (between 0 and 1) for the confidence
+##' interval. Defaults to \code{0.3}.
+##' @param removeFirst omit \code{removeFirst} values on the low frequency side.
+##' @param removeLast omit \code{removeLast} values on the high frequency side.
+##' @param xlab character string for labelling the x-axis.
+##' @param ylab character string for labelling the y-axis.
 ##' @param xlim range of x-axis values; if \code{NULL} (the default) it is
-##'     calculated internally and automatically reversed for \code{bPeriod =
-##'     TRUE}.
+##'     calculated internally and automatically reversed for
+##'     \code{bPeriod = TRUE}.
 ##' @param ylim range of y-axis values; if \code{NULL} (the default) it is
 ##'     calculated internally.
-##' @param ... other parameters to be passed to the line functio
-##' @return none
+##' @param ... further graphical parameters passed to \code{plot}.
 ##' @examples
-##' x<-ts(arima.sim(list(ar = 0.9),1000))
-##' spec<-SpecMTM(x)
-##' LPlot(spec,col="grey")
-##' LLines(LogSmooth(spec),lwd=2)
+##' x <- ts(arima.sim(list(ar = 0.9), 1000))
+##' spec <- SpecMTM(x)
+##' LPlot(spec, col = "grey")
+##' LLines(LogSmooth(spec), lwd = 2)
 ##' @author Thomas Laepple
 ##' @export
-LPlot<-function(x,conf=TRUE,bPeriod=FALSE,col="black",alpha=0.3,removeFirst=0,removeLast=0,xlab="f",ylab="PSD",xlim=NULL,ylim=NULL,...)
-{
+LPlot <- function(x, conf = TRUE, bPeriod = FALSE, col = "black", alpha = 0.3,
+                  removeFirst = 0, removeLast = 0, xlab = "f", ylab = "PSD",
+                  xlim = NULL, ylim = NULL, ...) {
     
     if (bPeriod) {
         x$freq <- 1/x$freq
@@ -35,16 +37,21 @@ LPlot<-function(x,conf=TRUE,bPeriod=FALSE,col="black",alpha=0.3,removeFirst=0,re
         xlab <- "period"
     }
 
-    index<-(removeFirst+1):(length(x$freq)-removeLast)
+    index <- (removeFirst + 1) : (length(x$freq) - removeLast)
     
-    x$freq<-x$freq[index]
-    x$spec<-x$spec[index]
-    x$lim.1<-x$lim.1[index]
-    x$lim.2<-x$lim.2[index]
+    x$freq  <- x$freq[index]
+    x$spec  <- x$spec[index]
+    x$lim.1 <- x$lim.1[index]
+    x$lim.2 <- x$lim.2[index]
 
-    plot(x$freq,x$spec,type="l",log="xy",col=col,xlab=xlab,ylab=ylab,xlim=xlim,ylim=ylim,...)
+    plot(x$freq, x$spec, type = "l", log = "xy", col = col, xlab = xlab,
+         ylab = ylab, xlim = xlim, ylim = ylim, ...)
   
-    if (conf) polygon(c(x$freq,rev(x$freq)),c(x$lim.1,
-        rev(x$lim.2)),col=ColTransparent(col,alpha),border=NA)
-    lines(x$freq,x$spec,col=col,...)
+    if (conf) {
+        polygon(c(x$freq, rev(x$freq)), c(x$lim.1, rev(x$lim.2)),
+                col = ColTransparent(col, alpha), border = NA)
+    }
+
+    lines(x$freq, x$spec, col = col, ...)
+    
 }
