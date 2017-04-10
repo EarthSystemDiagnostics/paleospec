4c729d3255b4decc4d3516475d4e72bedde0dc8a
Thom Laepple <tlaepple@awi.de>
2017-04-10 14:56:10 +0200

    added inverse filter function

---
+++ R/InverseFilter.R
@@ -0,0 +1,16 @@
+
+##' @title Construct the inverse filter in the time domain
+##' @param filter.weights 
+##' @return filter weights for the inverse filter
+##' @author Thomas Laepple
+##' @export
+InverseFilter <- function(filter.weights) {
+     if ((length(filter.weights)%%2) == 0) 
+        stop("N must be odd")
+    n <- length(filter.weights)
+    directtransfer <- rep(0, n)
+    directtransfer[(n + 1)/2] <- 1
+    filter.weights.inv <- directtransfer - filter.weights
+    return(filter.weights.inv)
+}
+
