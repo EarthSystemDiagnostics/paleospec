c941c036db9b5ce2e563a59b20dea9943d606a98
Thomas Münch <tmuench@awi.de>
2017-05-03 11:41:37 +0200

    'LPlot': Introduced the flags 'bNoPlot' and 'axes'.
    'bNoPlot = TRUE' suppresses the plotting, thus enables 'type = "n"' behaviour
    of the 'LPlot' function; 'axes = FALSE' suppresses plotting of the x and y
    axes. By using both flags only the empty plot frame can be created similar to
    the standard 'plot' function.

--- R/LPlot.R
+++ R/LPlot.R
@@ -7,6 +7,10 @@
 ##' interval (suppressed if \code{x} contains no error limits).
 ##' @param bPeriod if \code{TRUE} the x-axis is displayed in units of period
 ##'     (inverse frequency), increasing to the left. Defaults to \code{FALSE}.
+##' @param bNoPlot if \code{TRUE} only produce the plot frame (\code{type = "n"}
+##' behaviour of function \code{\link{plot}}). Defaults to \code{FALSE}.
+##' @param axes if \code{FALSE} the plotting of the x and y axes is
+##' suppressed. Defaults to \code{TRUE}.
 ##' @param col color for the line plot and the confidence interval.
 ##' @param alpha transparency level (between 0 and 1) for the confidence
 ##' interval. Defaults to \code{0.3}.
@@ -27,9 +31,9 @@
 ##' LLines(LogSmooth(spec), lwd = 2)
 ##' @author Thomas Laepple
 ##' @export
-LPlot <- function(x, conf = TRUE, bPeriod = FALSE, col = "black", alpha = 0.3,
-                  removeFirst = 0, removeLast = 0, xlab = "f", ylab = "PSD",
-                  xlim = NULL, ylim = NULL, ...) {
+LPlot <- function(x, conf = TRUE, bPeriod = FALSE, bNoPlot = FALSE, axes = TRUE,
+                  col = "black", alpha = 0.3, removeFirst = 0, removeLast = 0,
+                  xlab = "f", ylab = "PSD", xlim = NULL, ylim = NULL, ...) {
     
     if (bPeriod) {
         x$freq <- 1/x$freq
@@ -45,14 +49,14 @@ LPlot <- function(x, conf = TRUE, bPeriod = FALSE, col = "black", alpha = 0.3,
     x$lim.2 <- x$lim.2[index]
 
     plot(x$freq, x$spec, type = "n", log = "xy", xlab = xlab, ylab = ylab,
-         xlim = xlim, ylim = ylim, ...)
+         xlim = xlim, ylim = ylim, axes = axes, ...)
 
     lim <- !is.null(x$lim.1) & !is.null(x$lim.2)
-    if (conf & lim) {
+    if (conf & lim & !bNoPlot) {
         polygon(c(x$freq, rev(x$freq)), c(x$lim.1, rev(x$lim.2)),
                 col = ColTransparent(col, alpha), border = NA)
     }
 
-    lines(x$freq, x$spec, col = col, ...)
+    if (!bNoPlot) lines(x$freq, x$spec, col = col, ...)
     
 }
