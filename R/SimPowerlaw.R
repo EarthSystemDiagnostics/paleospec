2167cb3700961705f3817dcb2efb65b8b66831ed
Andrew Dolman <andydolman@gmail.com>
2017-07-04 13:49:51 +0200

    speed up SimPowerLaw by using vector of length power of 3

--- R/SimPowerlaw.R
+++ R/SimPowerlaw.R
@@ -17,22 +17,22 @@ AnPowerlaw<-function(beta,freq,return.scaling=FALSE)
 ##'
 ##' Method: FFT white noise, rescale, FFT back, the result is scaled to variance 1
 ##' @title Simulate a random timeseries with a powerlaw spectrum
-##' @param beta slope 
+##' @param beta slope
 ##' @param N length of timeseries to be generated
 ##' @return vector containing the timeseries
 ##' @author Thomas Laepple
 ##' @export
-SimPowerlaw<-function(beta,N)
+SimPowerlaw <- function(beta, N)
 {
-    Norg<-N
-    N<-ceiling(N/2)*2
-    df  = 1/(N);
-    f=seq(from=df,to=1/(2),by=df)
-    Filter=sqrt(1/(f^beta));
-    Filter = c(max(Filter), Filter,rev(Filter))
-    x   = scale(rnorm(N+1,1)) 
-    fx  =fft(x)
-    ffx =fx*Filter;
-    result<-scale(Re(fft(ffx,inverse=TRUE)))
-    return(result[1:Norg])
+  N2 <- (3^ceiling(log(N, base = 3)))
+  df  <- 1 / N2
+  f <- seq(from = df, to = 1/2, by = df)
+  Filter <- sqrt(1/(f^beta))
+  Filter <- c(max(Filter), Filter, rev(Filter))
+  x   <- scale(rnorm(N2, 1))
+  fx  <- fft(x)
+  ffx <- fx * Filter
+  result <- scale(Re(fft(ffx, inverse = TRUE)))
+  return(result[1:N])
 }
+
