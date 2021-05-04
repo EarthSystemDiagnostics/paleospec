c3e81ff8ede5fa2a0166e8e38b6c711aec312a74
Thomas Münch <tmuench@awi.de>
2021-05-04 10:41:06 +0200

    handle leading, trailing and internal NAs.

--- R/ApplyFilter.R
+++ R/ApplyFilter.R
@@ -1,13 +1,18 @@
 #' Filter time series
 #'
-#' Apply a given filter to a time series using different endpoint constraints as
-#' described below.
+#' Apply a given filter to a time series using different endpoint constraints.
 #'
 #' Note that when passing objects of class \code{ts}, the time step provided is
 #' not used; thus, for time series with a time step different from 1, the filter
 #' has to be adapted accordingly.
 #'
-#' The function applies endpoint constrains following Mann et al., GRL, 2003;
+#' Leading and trailing NA values are automatically stripped from the input
+#' vector so that they do not spread into the filtered data when applying the
+#' endpoint constraints, but added in again after filtering so that the output
+#' vector has the same length as the input. This does not apply to any internal
+#' NA values, which instead are handled by \code{na.rm}.
+#'
+#' The function applies endpoint constrains following Mann et al., GRL, 2004;
 #' available methods are:
 #' \itemize{
 #'   \item method = 0: no constraint (loss at both ends);
@@ -21,8 +26,15 @@
 #' @param filter numeric vector of filter weights.
 #' @param method single integer for choosing an endpoint constraint method;
 #'   available choices are integers 0-4, see details.
-#' @return filtered timeseries as a ts object.
+#' @param na.rm logical; control the handling of internal NA values in
+#'   \code{data}. If set to \code{TRUE}, any internal NA values are removed by
+#'   linear interpolation from the neighbouring values; defaults to
+#'   \code{FALSE}.
+#' @return a ts object with the filtered timeseries.
 #' @author Thomas Laepple
+#' @source The endpoint constraint methods are based on the study:\cr
+#'   Michael E. Mann, On smoothing potentially non‐stationary climate time
+#'   series, Geophys. Res. Lett., 31, L07214, doi:10.1029/2004GL019569, 2004.
 #' @examples
 #' # Simple running mean filter across three bins
 #' 
@@ -34,11 +46,32 @@
 #'
 #' # circular filtering avoids end losses, so as the other methods
 #' ApplyFilter(x, filter, method = 4)
+#'
+#' # leading and trailing NA's are ignored but added in again afterwards
+#' x <- c(NA, 1 : 10, NA)
+#' ApplyFilter(x, filter, method = 4)
+#'
+#' # ... but not internal NA's
+#' x <- c(1 : 5, NA, 7 : 10)
+#' ApplyFilter(x, filter, method = 4)
+#'
+#' # if not explicitly removed by linear interpolation
+#' ApplyFilter(x, filter, method = 4, na.rm = TRUE)
+#'
 #' @export
-ApplyFilter <- function(data, filter, method = 0) {
+ApplyFilter <- function(data, filter, method = 0, na.rm = FALSE) {
 
   if (!method %in% (0 : 4))
     stop("Unknown method; only 0 : 4 available.")
+
+  result <- rep(NA, length(data))
+
+  # remove leading and trailing NA's
+  x <- c(zoo::na.trim(data))
+  n <- length(x)
+
+  # linearly interpolate internal NA's if requested
+  if (na.rm) {x <- stats::approx(1 : n, x, 1 : n)$y}
 
   circular = FALSE
 
@@ -46,7 +79,7 @@ ApplyFilter <- function(data, filter, method = 0) {
 
     if (method == 4) {circular = TRUE}
 
-    result <- stats::filter(c(data), filter, circular = circular)
+    xf <- stats::filter(x, filter, circular = circular)
 
   } else {
 
@@ -54,26 +87,29 @@ ApplyFilter <- function(data, filter, method = 0) {
 
     if (method == 1) {
 
-      before <- rep(mean(data), N)
-      after  <- rep(mean(data), N)
+      before <- rep(mean(x), N)
+      after  <- rep(mean(x), N)
 
     } else if (method == 2 | method == 3) {
 
-      before <- c(data)[N : 1]
-      after  <- c(data)[length(data) : (length(data) - N + 1)]
+      before <- x[N : 1]
+      after  <- x[n : (n - N + 1)]
 
       if (method == 3) {
 
-        before <- c(data)[1] - (before - mean(before))
-        after  <- c(data)[length(data)] - (after - mean(after))
+        before <- x[1] - (before - mean(before))
+        after  <- x[n] - (after - mean(after))
 
       }
     }
 
-    result <- stats::filter(c(before, data, after), filter, circular = circular)
-    result <- result[(N + 1) : (N + length(data))]
+    xf <- stats::filter(c(before, x, after), filter, circular = circular)
+    xf <- xf[(N + 1) : (N + n)]
 
   }
+
+  i <- match(x[1], data) : match(x[n], data)
+  result[i] <- xf
 
   return(ts(result, frequency = frequency(data)))
 
--- tests/testthat/test-ApplyFilter.R
+++ tests/testthat/test-ApplyFilter.R
@@ -43,3 +43,42 @@ test_that("apply filter function works.", {
   expect_equal(actual, expected)
 
 })
+
+test_that("NA removal works.", {
+
+  # remove leading/trailing NA's
+  x <- c(NA, NA, 1 : 10, NA)
+  filter <- rep(1 / 3, 3)
+
+  expected <- ts(c(NA, NA, NA, 2 : 9, NA, NA))
+  actual <- ApplyFilter(x, filter, method = 0)
+
+  expect_equal(actual, expected)
+
+  expected <- ts(round(c(NA, NA, 13 / 3, 2 : 9, 20 / 3, NA), 2))
+  actual <- round(ApplyFilter(x, filter, method = 4), 2)
+
+  expect_equal(actual, expected)
+
+  # but not internal NA's
+  x <- c(1 : 5, NA, 7 : 10)
+
+  expected <- ts(c(NA, 2 : 4, NA, NA, NA, 8 : 9, NA))
+  actual <- ApplyFilter(x, filter, method = 0)
+
+  expect_equal(actual, expected)
+
+  # ... if the internal NA's are not explicitly interpolated
+  expected <- ts(c(NA, 2 : 9, NA))
+  actual <- ApplyFilter(x, filter, method = 0, na.rm = TRUE)
+
+  expect_equal(actual, expected)
+
+  # test combined case
+  x <- c(NA, 1 : 11, NA, 13 : 14, NA, NA, 17 : 20, rep(NA, 3))
+  expected <- ts(c(NA, NA, 2 : 19, NA, rep(NA, 3)))
+  actual <- ApplyFilter(x, filter, method = 0, na.rm = TRUE)
+
+  expect_equal(actual, expected)
+
+})
