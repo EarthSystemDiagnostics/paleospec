2aa321055098d66677e7776a7f0321bc0f10162f
andrewdolman <andydolman@gmail.com>
2022-07-19 23:47:55 +0200

    tidy code

--- R/CrossSpecMTM.R
+++ R/CrossSpecMTM.R
@@ -1,13 +1,26 @@
 #' @title MTM cross spectral estimator
-#' @description estimates cross spectral densities using slepian tapers with adaptive weighting. Function is an adaption of Peter Huybers Matlab coherence function cmtm.m
+#' @description estimates cross spectral densities using slepian tapers with
+#'   adaptive weighting. Function is an adaptation of Peter Huybers Matlab
+#'   coherence function cmtm.m
 #' @param x,y input data vectors (or array if y=NULL)
 #' @param dt time stepping (default = 1)
-#' @param nw slepian bandwidth parameter, typically something between 2.0 (default) and 8.0
-#' @param k The number of Slepian tapers used, often (default = NULL). For default NULL (2*nw -1) is used except for the cases 2*nw > N
+#' @param nw slepian bandwidth parameter, typically something between 2.0
+#'   (default) and 8.0
+#' @param k The number of Slepian tapers used, often (default = NULL). For
+#'   default NULL (2*nw -1) is used except for the cases 2*nw > N
 #' @param detrend (default=T)
 #'
 #' @return spectra object
 #' @export
+#' @examples
+#' x <- SimPowerlaw(1, 1e03)
+#' y <- ApplyFilter(x, filter = dnorm(-5:5, 0, 1), method  =3)
+#' cs1 <- CrossSpecMTM(x, y)
+#' plot(cs1$freq[1:length(x)], cs1$coh, log = "xy", type = "l")
+#'
+#' ch1 <- Coherence(x, y)
+#' lines(ch1$freq, ch1$coh, col = "red")
+#'
 CrossSpecMTM <- function(x,y=NULL,dt=1,k=NULL,nw=2,detrend=T) {
 
 # nw      - number of windows (default = 8)
@@ -53,7 +66,7 @@ CrossSpecMTM <- function(x,y=NULL,dt=1,k=NULL,nw=2,detrend=T) {
     P <- (Pk[,1]+Pk[,2])/2 # initial spectrum estimate
     Ptemp <-array(data=0,dim=c(N))
     P1 <- Ptemp
-    tol  <- .0005*vari[1,1]/N          # usually within 'tol'erance in about three iterations
+    tol  <- .0005*vari[1,1]/N          # usually within tolerance in about three iterations
     a <-vari[1,1]*abs(1-V)
     while ( sum(abs(P-P1)/N) > tol ) {
       b=(P%*%t(array(1,dim=c(k)))/(P%*%t(V)+array(1,dim=c(N))%*%t(a))) # weights
@@ -85,7 +98,7 @@ CrossSpecMTM <- function(x,y=NULL,dt=1,k=NULL,nw=2,detrend=T) {
                     taper="dpss",
                     v=v)
 
-  spec.out <- list(spec = Cxy[pls], # return ,spec' instead of crossspec to allow further handling e.g. with LogSmooth...
+  spec.out <- list(spec = Cxy[pls], # return spec instead of crossspec to allow further handling e.g. with LogSmooth...
                    specs = cbind(Fx[pls],Fy[pls]), # individual specs....
                    coh = c,
                    ph = ph,
