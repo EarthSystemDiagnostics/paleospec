37aaa6b402a75ba4b7837f991c034fe236898995
andrewdolman <andydolman@gmail.com>
2023-11-19 21:56:12 +0100

    add addtional tests for FilterSpecLog

--- tests/testthat/test-FilterSpec.R
+++ tests/testthat/test-FilterSpec.R
@@ -9,10 +9,7 @@ context("FilterSpec Function Tests")
 
 # Helper function to create a mock 'spec' object
 create_mock_spec <- function() {
-  list(freq = seq(0, 1, length.out = 100),
-       spec = rnorm(100),
-       dof = rep(5, 100),
-       shape = rep(2.5, 100))
+  SpecMTM(ts(rnorm(100)))
 }
 
 # Test with standard input
@@ -25,8 +22,22 @@ test_that("FilterSpec returns correct structure with standard inputs", {
   expect_equal(length(result$freq), length(spec$freq))
   expect_equal(length(result$spec), length(spec$spec))
   expect_equal(length(result$dof), length(spec$dof))
-  expect_equal(length(result$shape), length(spec$shape))
 })
+
+# Test dof length 1
+test_that("FilterSpec copes with dof length 1", {
+  spec <- create_mock_spec()
+
+  spec$dof <- spec$dof[1]
+
+  spans <- c(3, 5, 7) # Largest span is 7
+
+  result <- FilterSpec(spec, spans, method = 3, keep_low_f = TRUE)
+
+  expect_true(is.list(result))
+  expect_equal(length(result$dof), length(spec$freq))
+})
+
 
 # Test different methods
 test_that("FilterSpec handles different methods correctly", {
@@ -77,4 +88,76 @@ test_that("FilterSpec with method 0 adjusts length of result$freq", {
 })
 
 
-# Add more tests as necessary...
+
+# FilterSpecLog ------
+
+library(testthat)
+library(PaleoSpec) # Assuming PaleoSpec contains the FilterSpecLog function
+
+context("FilterSpecLog Function Tests")
+
+
+# Test basic functionality
+test_that("FilterSpecLog returns correct structure", {
+  spec <- create_mock_spec()
+  result <- FilterSpecLog(spec)
+
+  expect_true(is.list(result))
+  expect_equal(length(result$freq), length(spec$freq))
+  expect_equal(length(result$spec), length(spec$spec))
+  expect_equal(length(result$dof), length(spec$dof))
+})
+
+# Test dof length 1
+test_that("FilterSpecLog copes with dof length 1", {
+  spec <- create_mock_spec()
+
+  spec$dof <- spec$dof[1]
+
+  spans <- c(3, 5, 7) # Largest span is 7
+
+  result <- FilterSpecLog(spec)
+
+  expect_true(is.list(result))
+  expect_equal(length(result$dof), length(spec$freq))
+})
+
+
+# Test different methods
+test_that("FilterSpecLog handles different methods correctly", {
+  spec <- create_mock_spec()
+
+  for (m in 0:3) {
+    result <- FilterSpecLog(spec, method = m)
+    expect_true(is.list(result))
+  }
+})
+
+# Test df.log parameter
+test_that("FilterSpecLog handles df.log parameter correctly", {
+  spec <- create_mock_spec()
+  result_default <- FilterSpecLog(spec)
+  result_custom <- FilterSpecLog(spec, df.log = 0.1)
+
+  expect_true(is.list(result_default))
+  expect_true(is.list(result_custom))
+  expect_false(identical(result_default$spec, result_custom$spec))
+})
+
+# Test edge cases
+test_that("FilterSpecLog handles edge cases appropriately", {
+  empty_spec <- list(freq = numeric(0), spec = numeric(0), dof = numeric(0), shape = numeric(0))
+
+  expect_error(FilterSpecLog(empty_spec))
+  expect_error(FilterSpecLog("not a spec"))
+})
+
+# Test specific behaviour for method = 0
+test_that("FilterSpecLog with method 0 adjusts length of result$freq", {
+  spec <- create_mock_spec()
+
+  result <- FilterSpecLog(spec, method = 0)
+
+  expect_lt(length(result$freq), length(spec$freq))
+})
+
