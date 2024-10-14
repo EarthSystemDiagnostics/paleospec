7fe7ac5f5a759a9dcd88603f08720bda09a0ed38
andrewdolman <andydolman@gmail.com>
2024-10-14 17:11:14 +0200

    update gg_spec for linetype

--- R/gg_spec.R
+++ R/gg_spec.R
@@ -6,15 +6,17 @@
 #' @param conf Plot shaded confidence interval if it exists in the spec object
 #' @param alpha.line Alpha level for the spectra line(s)
 #' @param alpha.ribbon Alpha level for the confidence region(s)
-#' @param colour Colour or name of variable to which to map to colour
-#' @param min.colours minimum number of spectra before starting to colour them
+#' @param colour Name of variable map to colour, unquoted
+#' @param linetype Name of variable map to linetype, unquoted
+#' @param group Name of variable to map to group, unquoted
+#' @param min.colours Minimum number of spectra before starting to colour them
 #' separately
-#' @param removeFirst,removeLast remove first or last "n" values on the low or
-#'   high frequency side respectively. Will be unpredictable when used with a
-#'   list of spectra, or spec_df object with multiple spectra with different
-#'   frequency axes.
-#' @param force.CI Force the plotting of confidence regions when the total number
-#' of frequencies exceeds 10000. Defaults to FALSE
+#' @param removeFirst,removeLast Remove first or last "n" values on the low or
+#'   high frequency side respectively. Operates on a per group basis.
+#' @param force.lims,force.CI Force the plotting of confidence regions when the total number
+#' of frequencies exceeds 10000. force.CI is deprecated, use force.lims. Defaults to FALSE
+#' @param quantiles Plot uncertainty quantiles if they exist in the spec object
+#' @param time_unit Optional string giving the time unit for axes labels, e.g. "years"
 #'
 #' @return a ggplot object
 #' @family functions to plot power spectra
@@ -59,14 +61,25 @@
 #' sp2 <- LogSmooth(sp1)
 #' p <- gg_spec(sp2, p)
 #' p
-gg_spec <- function(x, gg = NULL, conf = TRUE,
-                    spec_id = NULL,
-                    colour = spec_id,
-                    alpha.line = 1,
-                    alpha.ribbon = 0.3,
-                    removeFirst = 0, removeLast = 0,
-                    force.CI = FALSE,
-                    min.colours = 2) {
+gg_spec <- function(x, gg = NULL,
+                     conf = TRUE,
+                     spec_id = NULL,
+                     colour = spec_id,
+                     group = spec_id,
+                     linetype = NULL,
+                     alpha.line = 1,
+                     alpha.ribbon = c(0.166, 0.333),
+                     removeFirst = 0, removeLast = 0,
+                     min.colours = 2,
+                     force.lims = FALSE,
+                     force.CI = FALSE,
+                     quantiles = FALSE,
+                     time_unit = NULL) {
+
+  if (!missing("force.CI")){
+    warning("the force.CI argument is deprecated please use force.lims")
+    force.lims <- force.CI
+  }
 
   gg_installed <- requireNamespace("ggplot2", quietly = TRUE)
 
@@ -86,45 +99,38 @@ gg_spec <- function(x, gg = NULL, conf = TRUE,
   }
 
 
-  ## Remove first and or last freqs on a per spec_id basis
+  if (removeFirst > 0) {
+    df <- df %>%
+      group_by({{group}}) %>%
+      filter(rank(df$freq) > removeFirst)
+
+    #[rank(df$freq) > removeFirst, ]
+  }
+
+  if (removeLast > 0) {
+    df <- df[rank(-df$freq) > removeLast,]
+  }
 
   if (exists("spec_id", df) == FALSE){
     df$spec_id <- 1
   }
 
+
   if (is.numeric(df$spec_id)){
     df$spec_id <- as.character(df$spec_id)
   }
 
-
-  if (removeFirst > 0) {
-    #df <- df[rank(df$freq) > removeFirst, ]
-
-    # Convert 'df' to a data.table
-    data.table::setDT(df)
-
-    # Group by 'spec_id' and filter rows where 'rank(freq)' is greater than 'removeFirst'
-    df <- df[, .SD[rank(freq) > removeFirst], by = spec_id]
-
-    # return to data.frame
-    df <- as.data.frame(df)
-  }
-
-  if (removeLast > 0) {
-    #df <- df[rank(-df$freq) > removeLast,]
-    # Convert 'df' to a data.table
-    data.table::setDT(df)
-
-    # Group by 'spec_id' and filter rows where 'rank(-freq)' is greater than 'removeLast'
-    df <- df[, .SD[rank(-freq) > removeLast], by = spec_id]
-
-    # return to data.frame
-    df <- as.data.frame(df)
+  if (is.null(time_unit) == FALSE){
+    lab_timescale <- paste0("Timescale [", time_unit, "]")
+    lab_freq <- paste0("Frequency [1/", time_unit, "]")
+  } else {
+    lab_timescale <- "Timescale"
+    lab_freq <- "Frequency"
   }
 
 
   if (is.null(gg)) {
-    p <- ggplot2::ggplot(data = df)
+    p <- ggplot2::ggplot(data = df, ggplot2::aes(group = {{group}}))
   } else {
     p <- gg
   }
@@ -133,27 +139,44 @@ gg_spec <- function(x, gg = NULL, conf = TRUE,
   df$PSD <- df$spec
   df$Frequency <- df$freq
 
+  df <- as.data.frame(df)
+
   if (conf == TRUE & exists("lim.1", df)){
 
-    if (nrow(df) > 1e04 & force.CI == FALSE){
+    if (nrow(df) > 1e04 & force.lims == FALSE){
       warning("geom_ribbon is very slow when the number of points > 1e04, skipping the confidence region")
     } else {
 
       p <- p +
         ggplot2::geom_ribbon(data = df, ggplot2::aes(x = Frequency, ymin = lim.2,
-                                                     group = spec_id,
                                                      ymax = lim.1, fill = {{ colour }}),
-                             alpha = alpha.ribbon, colour = NA)
+                             alpha = alpha.ribbon[1], colour = NA)
+    }
+  }
+
+  if (quantiles == TRUE & exists("X2.5.", df)){
+
+    if (nrow(df) > 1e04 & force.lims == FALSE){
+      warning("geom_ribbon is very slow when the number of points > 1e04, skipping the confidence region")
+    } else {
+
+      p <- p +
+        ggplot2::geom_ribbon(data = df, ggplot2::aes(x = Frequency, ymin = `X2.5.`,
+                                                     ymax = `X97.5.`, fill = {{ colour }}),
+                             alpha = alpha.ribbon[1], colour = NA)+
+        ggplot2::geom_ribbon(data = df, ggplot2::aes(x = Frequency, ymin = `X15.9.`,
+                                                     ymax = `X84.1.`, fill = {{ colour }}),
+                             alpha = alpha.ribbon[2], colour = NA)
     }
   }
 
   p <- p + ggplot2::geom_line(data = df, ggplot2::aes(x = Frequency, y = PSD,
-                                                      group = spec_id,
+                                                      linetype = {{ linetype }},
                                                       colour = {{ colour }}),
                               alpha = alpha.line
   ) +
-    ggplot2::scale_x_continuous(trans = "log10",
-                                sec.axis = ggplot2::sec_axis(~ 1/., "Timescale")) +
+    ggplot2::scale_x_continuous(name = lab_freq, trans = "log10",
+                                sec.axis = ggplot2::sec_axis(~ 1/., lab_timescale, labels = scales::comma)) +
     ggplot2::scale_y_continuous(trans = "log10") +
     ggplot2::annotation_logticks(sides = "tlb") +
     ggplot2::theme_bw() +
@@ -162,19 +185,22 @@ gg_spec <- function(x, gg = NULL, conf = TRUE,
                                  palette = "Dark2",
                                  aesthetics = c("colour", "fill")) +
     ggplot2::theme(panel.grid.minor = ggplot2::element_blank()) +
-    ggplot2::scale_alpha()
+    ggplot2::scale_alpha() +
+    ggplot2::labs(linetype = "")
 
   g <- ggplot2::ggplot_build(p)
   colrs <- unlist(unique(sapply(g$data, function(x) unique(x["colour"])$colour)))
   colrs <- colrs[is.na(colrs) == FALSE]
   ncolrs <- length(colrs)
 
+
+
   if (ncolrs <= min.colours){
 
     p <- p + ggplot2::scale_colour_manual("", values = "black",
                                           aesthetics = c("colour", "fill"))
 
-    if (is.null({{ colour }})){
+    if (is.null({{ colour }}) & is.null({{ linetype }})){
       p <- p + ggplot2::theme(legend.position = "none")
     }
 
@@ -182,6 +208,5 @@ gg_spec <- function(x, gg = NULL, conf = TRUE,
 
   p
 }
-
 
 
